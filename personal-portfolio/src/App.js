5866a5491efb17787ea53e1b16aa19198b468f7d
arob3990 <115756254+arob3990@users.noreply.github.com>
2023-03-27 21:21:22 -0700

    Delete personal-portfolio directory

--- personal-portfolio/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-import './App.css'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-const App = () => <PortfolioContainer />;
-
-export default App;
